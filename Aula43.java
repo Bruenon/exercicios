4c50a39c1245f1136c36727952166b43181c63f5
PET0151182 <j.gmail.com>
2023-08-07 11:29:37 -0300

    palavra: ");         palavra = input.next();         System.out.println("A palavra digitada foi: " + palavra);         System.out.print("Digite outra palavra e um número inteiro: ");         palavra = input.next();         num=input.nextInt();         System.out.println("Você digitou: " + palavra +" "+ num);

--- Aula43.java
+++ Aula43.java
@@ -6,7 +6,13 @@ public class Aula43 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String palavra; int num;
-        System.out.print("Digite uma")
-        input.close();//aaaaaaaaaa
+        System.out.print("Digite uma palavra: ");
+        palavra = input.next();
+        System.out.println("A palavra digitada foi: " + palavra);
+        System.out.print("Digite outra palavra e um número inteiro: ");
+        palavra = input.next();
+        num=input.nextInt();
+        System.out.println("Você digitou: " + palavra +" "+ num);
+        input.close();
     }
 }
