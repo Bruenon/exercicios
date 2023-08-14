0e1af39b6818804b2d637731450e4e67e7e3f8b4
Bruenon <bruno_mennabkc@hotmail.com>
2023-08-14 10:45:56 -0300

    bruenon projeto

---
+++ brueno.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class brueno {
+
+    public static void main(String[] args) {
+        Pessoa bruno = new Pessoa("bruno", 17);
+        bruno.mudar_nome("pedro");
+
+    }
+}
+
+class Pessoa{
+    String nome;
+    int idade;
+
+    public Pessoa(String nome_arg, int idade_arg){
+        nome = nome_arg;
+        idade = idade_arg;
+    }
+    public void mudar_nome(String noovo_nome){
+        nome = noovo_nome;
+    }
+}
